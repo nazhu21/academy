f721554a0095f8e33cb24117315e27913c76ca33
Nazarbek <admin@Administrators-MacBook-Air.local>
2024-03-25 20:20:16 -0700

    Python100 - day1 completed

---
+++ Python100/1-band_name.py
@@ -0,0 +1,6 @@
+greetings = "Welcome to the band name generator.\nFor this, you would have to answer following questions:"
+print(greetings)
+city_name = input("1. Name of the city you grew up? \n--")
+pet_name = input("2. Name of your first pet? \n--")
+
+print("Your band name could be: " + city_name + " " + pet_name + "!")
---
+++ Python100/1-day.py
@@ -0,0 +1,31 @@
+
+# !! print  function
+# print("Hello World!\nHello Today!")
+# print("Hello " + "Angela")
+# input("What's your name?")
+# print("Hello " + input("What is your name? ") + "!")
+
+# !! input function
+# name = input()
+# print(len(name))
+# print(len(input())) # above code simplified
+
+# !! Variables
+# name = input("Your name: ")
+# length = len(name)
+# print(length)
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
